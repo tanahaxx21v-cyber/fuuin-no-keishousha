cf4e7e05025a2411b4b3a1b1bac3e265d6b11e4b
アンソニー (ライター) <anthony@alphanote.ai>
2026-06-17 08:09:03 +0900

    Revert "feat: GitHub Pages 自動デプロイ設定を追加"
    This reverts commit bf3cd69cd3759b5d41675d65bbb7cadbc08e35e3.

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,4 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  output: 'export',
-  basePath: '/fuuin-no-keishousha',
-  images: { unoptimized: true },
-}
+const nextConfig = {};
 
-export default nextConfig
+export default nextConfig;
